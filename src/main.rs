69d29f7b3485fb30f7d8149c68e4114745b231b6
KGB-8375 <27stopittj@gmail.com>
2022-11-11 15:58:12 -0500

    uart communication

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,20 @@
 #![no_std]
 #![no_main]
 
+//use cortex_m::prelude::_embedded_hal_serial_Read;
 use rp_pico as bsp;
 
 use bsp::entry;
 use defmt::*;
 use defmt_rtt as _;
-use embedded_hal::digital::v2::OutputPin;
+//use embedded_hal::digital::v2::OutputPin;
 use panic_probe as _;
 
 use bsp::hal::{
     clocks::{init_clocks_and_plls, Clock},
     pac,
     sio::Sio,
+    uart,
     watchdog::Watchdog,
 };
 
@@ -23,7 +25,7 @@ use bsp::hal::{
 fn main() -> ! {
     info!("Program start");
     let mut pac = pac::Peripherals::take().unwrap();
-    let core = pac::CorePeripherals::take().unwrap();
+    //let core = pac::CorePeripherals::take().unwrap();
     let mut watchdog = Watchdog::new(pac.WATCHDOG);
     let sio = Sio::new(pac.SIO);
 
@@ -41,7 +43,7 @@ fn main() -> ! {
     .ok()
     .unwrap();
 
-    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+    //let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
 
     let pins = bsp::Pins::new(
         pac.IO_BANK0,
@@ -50,14 +52,19 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    let mut led_pin = pins.led.into_push_pull_output();
+    //let mut led_pin = pins.led.into_push_pull_output();
+
+    let uart_pins = (pins.gpio0.into_mode(), pins.gpio1.into_mode());
+
+    let ibus = uart::UartPeripheral::new(pac.UART0, uart_pins, &mut pac.RESETS)
+        .enable(uart::UartConfig::default(), clocks.peripheral_clock.freq())
+        .unwrap();
 
     loop {
-        info!("on!");
-        led_pin.set_high().unwrap();
-        delay.delay_ms(500);
-        info!("off!");
-        led_pin.set_low().unwrap();
-        delay.delay_ms(500);
+        let mut buff = [0; 0x20];
+
+        if ibus.read_full_blocking(&mut buff).is_ok() {
+            info!("Read bytes {=[u8; 32]:02X}", buff);
+        }
     }
 }
